be46bf559b2c186afc9dd64ab679333eb0d85061
Vadim Marchenko <neprist@gmail.com>
2017-01-06 15:40:16 +0200

    commit 2

--- desktop/src/com/cocktail_games_inc/desktop/DesktopLauncher.java
+++ desktop/src/com/cocktail_games_inc/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "G_Falls";
-		config.width = 800;
-		config.height = 480;
+		config.width = 480;
+		config.height = 600;
 		new LwjglApplication(new G_falls(), config);
 	}
 }
